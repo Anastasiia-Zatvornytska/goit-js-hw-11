ac80c63ab645ddbf810318019c31c8dbe088c42a
Anastasiia-Zatvornytska <nastulja@gmail.com>
2026-09-07 03:37:43 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+// import ------ from './js/pixabay-api';
+// import ------ from './js/render-functions';
